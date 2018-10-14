27d1ff3a7f6b58e7e24f2cfcb4fb25ac38e7f502
kimdaehwan <silencepeach@korea.ac.kr>
2018-10-14 17:31:04 +0900

    modification

--- assignment2.js
+++ assignment2.js
@@ -25,21 +25,21 @@ function readFile(file, data) {
 writeFile("./1.txt", "11111")
 .then(function() {
     console.log("1.txt created");
-    return readFile("./1.txt", "UTP-8");
+    return readFile("./1.txt", "UTF-8");
 }).catch(function(err) {})
 .then(function(data1) {
     return writeFile("./2.txt", data1 + "22222");
 }).catch(function(err) {})
 .then(function() {
     console.log("2.txt created");
-    return readFile("./2.txt", "UTP-8");
+    return readFile("./2.txt", "UTF-8");
 }).catch(function(err) {})
 .then(function(data2) {
     return writeFile("./3.txt", data2 + "33333");
 }).catch(function(err) {})
 .then(function() {
     console.log("3.txt created");
-    return readFile("./3.txt", "UTP-8");
+    return readFile("./3.txt", "UTF-8");
 }).catch(function(err) {})
 .then(function(data3) {
     console.log(data3);
--- assignment3.js
+++ assignment3.js
@@ -8,14 +8,14 @@ const start = async() => {
     try{
         await writeFile2("./1.txt", "11111");
         console.log("1.txt created");
-        const data1 = await readFile2("./1.txt", "UTP-8");
+        const data1 = await readFile2("./1.txt", "UTF-8");
         await writeFile2("./2.txt", data1 + "22222");
         console.log("2.txt created");
-        const data2 = await readFile2("./2.txt", "UTP-8");
-        await writeFile3("./3.txt", data2 + "33333");
+        const data2 = await readFile2("./2.txt", "UTF-8");
+        await writeFile2("./3.txt", data2 + "33333");
         console.log("3.txt created");
-        const data3 = await readFile2("./3.txt", "UTP-8");
-        cosole.log(data3);
+        const data3 = await readFile2("./3.txt", "UTF-8");
+        console.log(data3);
     } catch(err) {
         console.log(err);
     }
